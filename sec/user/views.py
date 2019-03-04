24cccc70d2f21870a48e37fc4c6f3e32c5671015
Marius Kohmann <marius.kohmann@gmail.com>
2019-03-04 12:21:42 +0100

    add conf email to sign up

--- sec/user/views.py
+++ sec/user/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django.contrib.sessions.backends.cache import SessionStore
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, FormView
 from django.contrib.auth import authenticate
+from django.core.mail import EmailMessage
 from .forms import SignUpForm, LoginForm
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
 
 from ratelimit.mixins import RatelimitMixin
 
@@ -47,6 +54,7 @@ class SignupView(CreateView):
     template_name = "user/signup.html"
     success_url = reverse_lazy("home")
 
+
     def form_valid(self, form):
         user = form.save()
 
@@ -54,8 +62,37 @@ class SignupView(CreateView):
         user.profile.company = form.cleaned_data["company"]
         user.profile.categories.add(*categories)
 
+        user.is_active = False
+
         user.save()
 
-        login(self.request, user)
+        message = render_to_string('user/acc_active_email.html', {
+            'user': user,
+            'domain': "127.0.0.1:8000",
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+            'token': account_activation_token.make_token(user),
+        })
 
-        return HttpResponseRedirect(self.success_url)
+        to_email = form.cleaned_data.get('email')
+        email = EmailMessage(
+            "Confirm email", message, to=[to_email]
+        )
+        email.send()
+        return HttpResponse('Please confirm your email address to complete the registration')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        print(uid)
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
